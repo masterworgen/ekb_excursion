516233f900da897340bbec96432e2f9eb4cab85f
Gleb <melkitasit@yandex.ru>
2019-01-31 21:42:24 +0500

    test

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from blog.models import Blog
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    news = Blog.objects.all()
+    return render(request, "index.html", {"news": news})
 
 
 def feedback(request):
     return HttpResponse("<h2>Отзывы</h2>")
+
+
+def addNews(request):
+    if request.method == "POST":
+        news = Blog()
+        news.header = request.POST.get("name")
+        news.date_create = request.POST.get("age")
+        news.text = request.POST.get("text")
+        news.save()
+    return HttpResponseRedirect("/")
