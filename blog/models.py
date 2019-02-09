43373a358fa311ffcd235f21e3a5664a083f1e69
Gleb <melkitasit@yandex.ru>
2019-02-09 15:27:28 +0500

    Начал рааботу отзывов

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,10 @@ class Blog(models.Model):
     header = models.CharField(max_length=40)
     date_create = models.DateTimeField(auto_now_add=True)
     text = models.TextField(blank=True)
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=20)
+    date_create = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=True)
+    verification = models.BooleanField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from blog.models import Blog
+from blog.models import Blog, Feedback
 
 
 def get_response(request, template, params):
@@ -18,14 +18,20 @@ def index(request):
 
 def news(request, topic_id=None):
     if topic_id is not None:
-        print("news/" + topic_id)
         article = Blog.objects.get(id=topic_id)
         return get_response(request, 'news_article.html', {"news_article": article})
     else:
-        print("news")
         news_list = Blog.objects.all()
         return get_response(request, "news_list.html", {"news_list": news_list})
 
 
 def feedback(request):
-    return get_response(request, "feedback.html", {})
+    fb = Feedback(request.POST)
+    if request.method == "POST":
+        fb.name = request.POST.get("name")
+        fb.date_create = request.POST.get("dc")
+        fb.text = request.POST.get("text")
+        fb.verification = 0
+        return render(request, "feedback.html")
+    elif request.method == "GET":
+        return render(request, "feedback.html", {"form": fb})
