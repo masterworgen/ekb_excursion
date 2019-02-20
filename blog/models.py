61ef8fa44f6443508237ecfb3b3e7493a0a0eb61
Gleb <melkitasit@yandex.ru>
2019-02-20 17:23:12 +0500

    Создал в БД таблицы для экскурсий

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Blog(models.Model):
     header = models.CharField(max_length=40)
     date_create = models.DateTimeField(auto_now_add=True)
-    text = models.TextField(blank=True)
+    text = RichTextUploadingField(blank=True, default='')
 
 
 class Feedback(models.Model):
     name = models.CharField(max_length=20)
     date_create = models.DateTimeField(auto_now_add=True)
-    text = models.TextField(blank=True)
+    text = RichTextUploadingField(blank=True, default='')
     verification = models.BooleanField()
 
 
 class Excursions(models.Model):
     name = models.CharField(max_length=50)
-    text = models.TextField(blank=True)
-    json = models.TextField(blank=True)
+    text = RichTextUploadingField(blank=True, default='')
+    json = RichTextField(blank=True, default='')
