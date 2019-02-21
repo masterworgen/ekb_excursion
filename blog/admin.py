1dc0c3a4d86516836955d0179ba59ef32e7a012b
Gleb <melkitasit@yandex.ru>
2019-02-21 14:20:30 +0500

    Добавил в админку экскурсии

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Blog, Feedback
+from .models import Blog, Feedback, Excursions
 
 admin.site.register(Blog)
 admin.site.register(Feedback)
+admin.site.register(Excursions)
