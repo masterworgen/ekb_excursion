9fba81bfedbef1442508a15bc074d7d2746ac680
Gleb <melkitasit@yandex.ru>
2019-02-26 21:18:25 +0500

    Добавил отправку письма при заявки на экскурсию

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from blog.models import Blog, Feedback, Excursion
-
+from django.core.mail import send_mail
 
 def get_response(request, template, params):
     if request.method == "GET" and "content" in request.GET and request.GET["content"]:
@@ -26,6 +26,11 @@ def news(request, topic_id=None):
 
 def excursions(request):
     excursions_list = Excursion.objects.all()
+    if request.method == "POST":
+        exc = Excursion.objects.get(id=request.POST.get("excursionId")).name
+        text = request.POST.get("name") + " записался на экскурсию: " + exc + "\nПочта: "+request.POST.get("email")+"\nТелефон: "+request.POST.get("tel")
+        send_mail('Запись на эксурсию', text, 'excurison@екб-прогулка.рф',
+                  ['excurison@екб-прогулка.рф'], fail_silently=False)
     return get_response(request, "excursions.html", {"excursions": excursions_list})
 
 
